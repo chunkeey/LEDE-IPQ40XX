a741e96b9f219370ec5e866b123b39088c84d6b4
Christian Lamparter <chunkeey@gmail.com>
2016-11-19 03:20:55 +0100

    kernel: move dwc3-of-simple definition
    The ipq4019 also needs the dwc3-of-simple.
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- target/linux/ipq806x/modules.mk
+++ target/linux/ipq806x/modules.mk
@@ -1,19 +1,3 @@
-define KernelPackage/usb-dwc3-of-simple
-  TITLE:=DWC3 USB simple OF driver
-  DEPENDS:=+kmod-usb-dwc3
-  KCONFIG:= CONFIG_USB_DWC3_OF_SIMPLE
-  FILES:= $(LINUX_DIR)/drivers/usb/dwc3/dwc3-of-simple.ko
-  AUTOLOAD:=$(call AutoLoad,53,dwc3-of-simple,1)
-  $(call AddDepends/usb)
-endef
-
-define KernelPackage/usb-dwc3-of-simple/description
- This driver provides generic platform glue for the integrated DesignWare
- USB3 IP Core.
-endef
-
-$(eval $(call KernelPackage,usb-dwc3-of-simple))
-
 define KernelPackage/usb-phy-qcom-dwc3
   TITLE:=DWC3 USB QCOM PHY driver
   DEPENDS:=@TARGET_ipq806x +kmod-usb-dwc3-of-simple
