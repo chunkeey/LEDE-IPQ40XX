9471fe22f3ef3d0b0c818d370cebecf7ea467f96
Christian Lamparter <chunkeey@gmail.com>
2016-11-19 19:15:31 +0100

    ipq40xx: add ipq40xx target
    This target is based on previous work from:
    commit 0070970b6c85939344ec5c6f538c91a84177217a
    Author: Christian Mehlis <christian@m3hlis.de>
    Date:   Fri Nov 4 22:42:36 2016 +0100
    
        added ipq40xx target
    
    which is a copy of the ipq806x target
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

---
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,29 @@
+. /lib/ipq40xx.sh
+
+PART_NAME=firmware
+
+platform_check_image() {
+	return 0;
+}
+
+platform_pre_upgrade() {
+	local board=$(ipq40xx_board_name)
+
+	case "$board" in
+		;;
+	esac
+}
+
+platform_do_upgrade() {
+	local board=$(ipq40xx_board_name)
+}
+
+platform_nand_pre_upgrade() {
+	local board=$(ipq40xx_board_name)
+}
+
+blink_led() {
+	. /etc/diag.sh; set_state upgrade
+}
+
+append sysupgrade_pre_upgrade blink_led
